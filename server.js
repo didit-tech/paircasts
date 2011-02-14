1f79b5a93dab019df6c8c68e4c8671aa00d7f1c8
Robert Malko <robmalko@gmail.com>
2011-02-14 08:11:05 -0500

    removed server.j

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-  , app = express.createServer();
-
-app.get('/', function(req, res){
-    res.send('<h1 style="font-size: 60px;">Paircasts coming soon...</h1>');
-});
-
-app.listen(3000);
